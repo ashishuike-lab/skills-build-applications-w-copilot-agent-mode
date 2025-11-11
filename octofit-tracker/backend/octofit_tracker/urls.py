02fb52c6acea481dc449d10a5778f2d016de18f5
ashishuike-lab <ashish.uike@hcl.com>
2025-11-11 09:20:44 +0000

    Redirect root URL to API root in Django urls.py

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,9 +13,39 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from octofit_tracker.views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'teams', TeamViewSet)
+router.register(r'activities', ActivityViewSet)
+router.register(r'workouts', WorkoutViewSet)
+router.register(r'leaderboard', LeaderboardViewSet)
+
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+BASE_URL = f"https://{CODESPACE_NAME}-8000.app.github.dev/api" if CODESPACE_NAME else "http://localhost:8000/api"
+
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'users': f'{BASE_URL}/users/',
+        'teams': f'{BASE_URL}/teams/',
+        'activities': f'{BASE_URL}/activities/',
+        'workouts': f'{BASE_URL}/workouts/',
+        'leaderboard': f'{BASE_URL}/leaderboard/',
+    })
+
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
+    path('api/', include(router.urls)),
+    path('', lambda request: redirect('api-root')),  # Redirect root to API root
 ]
